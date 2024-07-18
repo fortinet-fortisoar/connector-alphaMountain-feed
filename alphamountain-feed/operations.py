65ffd58d1f29126bc7da96b7373e8d0d0d8b3fc3
cs-shivaji-kolape <skolape@fortinet.com>
2024-07-18 23:15:31 +0530

    Added latest code

--- alphamountain-feed/operations.py
+++ alphamountain-feed/operations.py
@@ -20,12 +20,8 @@ class alphaMountainFeed(object):
         self.server_url = config.get('server_url', '').strip()
         if not self.server_url.startswith('https://') and not self.server_url.startswith('http://'):
             self.server_url = 'https://' + self.server_url
-        # need to remove
-        self.server_url = 'https://batch.alphamountain.ai'
         self.verify_ssl = config.get('verify_ssl', False)
-        # self.api_key = config.get('api_key')
-        # need to remove
-        self.api_key = '2c52b78c-626e-45e0-b263-ae6d30e8b380'
+        self.api_key = config.get('api_key')
         self.headers = {'Content-Type': 'application/json'}
         self.version = API_VERSION
         self.request_type = REQUEST_TYPE
@@ -95,11 +91,28 @@ def get_category_ids(categories):
     return [key for key, val in categories_resp.items() if val in categories]
 
 
+def get_categories_values(category_feeds):
+    available_category = get_available_category(response_type='dict')
+    for feed in category_feeds:
+        translated_categories  = []
+        categories = feed.get('categories', [])
+        translated_categories = []
+        for category in categories:
+            category_name = available_category.get(str(category))
+            if category_name:
+                translated_categories.append(category_name)
+            else:
+                translated_categories.append(str(category))  # Use category ID if name not found
+        feed['categories'] = translated_categories
+    return category_feeds
+
+
 def get_indicator_category(params, alpha_feed):
     try:
         payload = get_payload(params, alpha_feed)
         indicator_feed_with_categories = alpha_feed.make_api_call('/category/feed/json', payload=json.dumps(payload))
-        return indicator_feed_with_categories.get('feed', [])
+        category_feeds = indicator_feed_with_categories.get('feed', [])
+        return get_categories_values(category_feeds)
     except Exception as err:
         logger.error("Getting error while fetching indicators categories")
         logger.error(err)
@@ -135,7 +148,7 @@ def get_indicators(config, params):
     categories = params.pop('categories', '')
     payload = get_payload(params, alpha_feed)
     resp = alpha_feed.make_api_call('/threat/feed/json', payload=json.dumps(payload))
-    final_response = resp.get('feed')
+    indicator_feeds = resp.get('feed')
     risk_min = params.pop('risk_min', '')
     risk_max = params.pop('risk_max', '')
     if include_categories:
@@ -143,13 +156,14 @@ def get_indicators(config, params):
             categories_ids = get_category_ids(categories)
             params['categories'] = categories_ids
         indicator_feed_categories = get_indicator_category(params, alpha_feed)
-        final_response = build_response(final_response, indicator_feed_categories)
+        indicator_feeds = build_response(indicator_feeds, indicator_feed_categories)
     if include_popularity:
         categories = params.pop('categories', '')
         flags = params.pop('flags', '')
         feed_indicator_popularity = get_indicator_popularity(params, alpha_feed)
-        final_response = build_response(final_response, feed_indicator_popularity)
-    return final_response
+        indicator_feeds = build_response(indicator_feeds, feed_indicator_popularity)
+    resp['feed'] = indicator_feeds
+    return resp
 
 
 def _check_health(config):
